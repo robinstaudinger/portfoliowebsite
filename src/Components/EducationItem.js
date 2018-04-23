5d12226c0e0c6442fa481bfd94510de483f5aa19
Robin Staudinger <robin.staudinger@hotmail.com>
2018-04-23 20:39:21 +0300

    added gpa progressbars for education

--- src/Components/EducationItem.js
+++ src/Components/EducationItem.js
@@ -8,6 +8,7 @@ EducationItem returns the education item with proper styling.
 */
 
 import React, { Component } from 'react';
+import { ProgressBar } from 'react-bootstrap';
 
 export default class EducationItem extends Component {
   render() {
@@ -18,6 +19,15 @@ export default class EducationItem extends Component {
         <div class="dateText">{this.props.school.dates}</div>
         <div class="locationText">{this.props.school.location}</div>
         <em>Major: {this.props.school.major}</em>
+        <div class="gpa">
+        <p>GPA: </p>
+        {this.props.school.degree === "MSc" ? (
+          <ProgressBar active now={this.props.school.gpa} max={this.props.school.maxGrade} label={`${this.props.school.gpa} / ${this.props.school.maxGrade}` }/>
+      ) : (
+        <ProgressBar bsStyle="success" now={this.props.school.gpa} max={this.props.school.maxGrade} label={`${this.props.school.gpa} / ${this.props.school.maxGrade}` }/>
+
+      )}
+      </div>
         </div>
       );
     }
